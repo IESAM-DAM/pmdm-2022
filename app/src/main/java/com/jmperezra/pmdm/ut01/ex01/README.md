e7c0b3e7d55f75185f62112ebb247a636c008907
Chema <josem.perram@educa.jcyl.es>
2022-09-27 16:51:45 +0200

    added new exercise about LinearLayout

---
+++ app/src/main/java/com/jmperezra/pmdm/ut01/ex01/README.md
@@ -0,0 +1,2 @@
+# UT-01 Ejercicio 01
+Práctica con LinearLayouts
---
+++ app/src/main/java/com/jmperezra/pmdm/ut01/ex01/presentation/Ut01Ex01Activity.kt
@@ -0,0 +1,12 @@
+package com.jmperezra.pmdm.ut01.ex01.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jmperezra.pmdm.R
+
+class Ut01Ex01Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01ex01)
+    }
+}
