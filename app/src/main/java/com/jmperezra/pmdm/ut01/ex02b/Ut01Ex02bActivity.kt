5ed85f0f5b090a22500b4ba5dc98b7562e5d2c39
Chema Pramos <jmperezra@gmail.com>
2022-10-04 16:20:32 +0200

    Added new exercise regards to LinearLayout

--- app/src/main/java/com/jmperezra/pmdm/ut01/ex02b/Ut01Ex02bActivity.kt
+++ app/src/main/java/com/jmperezra/pmdm/ut01/ex02b/Ut01Ex02bActivity.kt
@@ -1,12 +1,12 @@
-package com.jmperezra.pmdm.ut01.ex02.presentation
+package com.jmperezra.pmdm.ut01.ex02b
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jmperezra.pmdm.R
 
-class Ut01Ex02Activity : AppCompatActivity() {
+class Ut01Ex02bActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ut01ex02)
+        setContentView(R.layout.activity_ut01ex02b)
     }
 }
