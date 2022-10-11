9ad507c891101578b215756d0ba7e618a8db0bfa
Chema Pramos <jmperezra@gmail.com>
2022-10-11 15:20:41 +0200

    updated project

---
+++ app/src/main/java/com/jmperezra/pmdm/ut01/ex03/Ut01Ex03Activity.kt
@@ -0,0 +1,12 @@
+package com.jmperezra.pmdm.ut01.ex03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jmperezra.pmdm.R
+
+class Ut01Ex03Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01ex03)
+    }
+}
